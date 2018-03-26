c5b3896b85fa811b68766d3c88b3dd8d5ebd73ff
MarcoAntonioUcChe <36580101+MarcoAntonioUcChe@users.noreply.github.com>
2018-03-26 14:18:56 -0600

    Actividades semana1

---
+++ Angular01/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ComponenteUnoComponent} from './componente-uno/componente-uno.component';
+import {ComponenteDosComponent} from './componente-dos/componente-dos.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+
+})
+export class AppComponent {
+  title = 'Modificar qui men!!!!!';
+}
---
+++ Angular01/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ComponenteUnoComponent } from './componente-uno/componente-uno.component';
+
+import { ComponenteDosComponent } from './componente-dos/componente-dos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponenteUnoComponent,
+    ComponenteUnoComponent,
+    ComponenteDosComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
